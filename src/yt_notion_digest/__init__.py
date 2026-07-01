27440b6cda93fe81218e66f9c65181dcaae2d4a2
univcorp2-ctrl <univcorp2@gmail.com>
2026-07-01 18:21:53 +0900

    Add package init

---
+++ src/yt_notion_digest/__init__.py
@@ -0,0 +1,5 @@
+"""YouTube channel to Notion knowledge-base pipeline."""
+
+from yt_notion_digest.models import Channel, PipelineResult, TranscriptResult, Video
+
+__all__ = ["Channel", "PipelineResult", "TranscriptResult", "Video"]
