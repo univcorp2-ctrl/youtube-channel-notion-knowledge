ef5daae56bdbc86836a983cafc3671a3c67de600
univcorp2-ctrl <univcorp2@gmail.com>
2026-07-02 18:12:46 +0900

    Add playlist source support and run requested Notion trial

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,4 +1,4 @@
-from yt_notion_digest.youtube import parse_channel_identity
+from yt_notion_digest.youtube import parse_channel_identity, parse_playlist_id
 
 
 def test_parse_handle_url_with_query() -> None:
@@ -17,3 +17,13 @@ def test_parse_channel_id_url() -> None:
 
 def test_parse_custom_url_falls_back_to_search() -> None:
     assert parse_channel_identity("https://www.youtube.com/c/SomeChannel") == ("search", "SomeChannel")
+
+
+def test_parse_playlist_id() -> None:
+    url = "https://youtube.com/playlist?list=PL0pHg9WQBbWZqnTu9vqMUdwsxRaZIB0Ja&si=abc"
+    assert parse_playlist_id(url) == "PL0pHg9WQBbWZqnTu9vqMUdwsxRaZIB0Ja"
+
+
+def test_parse_playlist_id_from_watch_url() -> None:
+    url = "https://www.youtube.com/watch?v=abc123&list=PLxyz"
+    assert parse_playlist_id(url) == "PLxyz"
